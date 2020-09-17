63b2ebdb0d2251d0150344e880123cdf40847652
kimmuzan <tureblue321@gmail.com>
2020-09-17 22:52:44 +0900

    add modify !청소

--- index.js
+++ index.js
@@ -128,14 +128,10 @@ client.on('message', (message) => {
     } else {
       return message.reply('채널에서 실행해주세요.');
     }
-  } else if(message.content.startsWith('!청소')) {
-    if(message.channel.type == 'dm') {
-      return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
-    }
-    
-    if(message.channel.type != 'dm' && checkPermission(message)) return
+  } if(message.content.startsWith('!청소')) {
+    if(checkPermission(message)) return
 
-    var clearLine = message.content.slice('!청소'.length);
+    var clearLine = message.content.slice('!청소 '.length);
     var isNum = !isNaN(clearLine)
 
     if(isNum && (clearLine <= 0 || 100 < clearLine)) {
@@ -147,9 +143,10 @@ client.on('message', (message) => {
 
         var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
         var count = parseInt(message.content.split(' ')[2])+1;
+        const _limit = 10;
         let _cnt = 0;
 
-        message.channel.fetchMessages().then(collected => {
+        message.channel.fetchMessages({limit: _limit}).then(collected => {
           collected.every(msg => {
             if(msg.author.id == user) {
               msg.delete();
